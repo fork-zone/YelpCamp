7a89f6a74630a0a563f6850336af254738a9eacc
Tony Brackins <mrbrackins@outlook.com>
2016-02-05 09:00:59 -0800

    Added header and footer partials and campgrounds page

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+
+// Set View Engine to EJS
+app.set("view engine", "ejs");
+
+//Homepage
+app.get("/", function(req, res){
+res.render("landing");
+});
+
+//Campgrounds page
+app.get("/campgrounds", function(req, res){
+	var campgrounds = [
+		{name: "Salmon Creek", image: "http://photosforclass.com/download/7962474612"},
+		{name: "Granite Hill", image: "http://photosforclass.com/download/215827008"},
+		{name: "Mountain Goat's Rest", image: "http://photosforclass.com/download/6234565071"}
+	]
+res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+//messing with OOP
+// var campground = function(campname, image) {
+// 	this.campname = campname;
+// 	this.campimage = campimage;
+// }
+
+
+
+
+//Listener for server
+app.listen("3000", function(){
+	console.log("The YelpCamp server has started!");
+});
