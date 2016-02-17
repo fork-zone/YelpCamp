2f672b809755d47f6e7e9a50cd65ab852e56b39c
Tony Brackins <mrbrackins@outlook.com>
2016-02-17 08:39:00 -0800

    adding from home

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express		= require("express"),
+	app 		= express(),
+	bodyParser 	= require("body-parser"),
+	mongoose 	= require("mongoose")  
+
+mongoose.connect("mongodb://tonybrackins:xxxtasy1@ds061365.mongolab.com:61365/yelp_camp");
+
 // var MongoClient = require('mongodb').MongoClient;  for Mongo DB
-var assert = require('assert');
+// var assert = require('assert');
+
+
 
 
 app.use(bodyParser.urlencoded({extended:true})); //memorize this line
+
+// Set View Engine to EJS
+app.set("view engine", "ejs");
+
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+//SCHEMA SETUP
+	// Model Setup
+var Campground = mongoose.model("Campground", campgroundSchema);;
+
+// Campground.create(
+// 	{
+// 		name: "Granite Hill", 
+// 		image: "http://photosforclass.com/download/215827008"
+
+// 	}, function(err, camp){
+// 		if(err) {
+// 			console.log("we caught an error");
+// 		} else {
+// 			console.log("we just created");
+// 			console.log(camp);
+// 		}
+// 	});
 
 //database:
 //Mongo
@@ -21,14 +53,13 @@ app.use(bodyParser.urlencoded({extended:true})); //memorize this line
 //Mongoose
 // var uri = 'mongodb://tonybrackins:xxxtasy1@ds060478.mongolab.com:60478/tonymongo';
 // global.db = mongoose.createConnection(uri);
-var campgrounds = [
-		{name: "Salmon Creek", image: "http://photosforclass.com/download/7962474612"},
-		{name: "Granite Hill", image: "http://photosforclass.com/download/215827008"},
-		{name: "Mountain Goat's Rest", image: "http://photosforclass.com/download/6234565071"},
-		{name: "Really Cool Campg", image: "http://www.makeyourdayhere.com/ImageRepository/Document?documentID=51"}
-	]
-// Set View Engine to EJS
-app.set("view engine", "ejs");
+// var campgrounds = [
+// 		{name: "Salmon Creek", image: "http://photosforclass.com/download/7962474612"},
+// 		{name: "Granite Hill", image: "http://photosforclass.com/download/215827008"},
+// 		{name: "Mountain Goat's Rest", image: "http://photosforclass.com/download/6234565071"},
+// 		{name: "Really Cool Campg", image: "http://www.makeyourdayhere.com/ImageRepository/Document?documentID=51"}
+// 	]
+
 
 //Homepage
 app.get("/", function(req, res){
@@ -37,8 +68,19 @@ res.render("landing");
 
 //Campgrounds page
 app.get("/campgrounds", function(req, res){
+	//get all campgrounds from DB
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log("bruhh, we got a problem");
+		} else{
+			console.log("Yo, we just FOUND this: ");
+			console.log(allCampgrounds);
+			res.render("campgrounds", {campgrounds: allCampgrounds});
+		}
+	});
 	
-res.render("campgrounds", {campgrounds:campgrounds});
+
+
 });
 
 app.post("/campgrounds", function(req, res) {
@@ -47,9 +89,18 @@ app.post("/campgrounds", function(req, res) {
 	var name = req.body.name;
 	var image = req.body.image;
 	var newCampground = {name:name, image:image};
-	campgrounds.push(newCampground);
+	// campgrounds.push(newCampground);
+	// create a new campground and save to database
+	Campground.create(newCampground, function(err,newlycreated){
+		if(err){
+		console.log("yo, got an error: " + err);
+		} else {
+			console.log("yo, just created: " + newlycreated);
+			res.redirect("/campgrounds");
+		}
+	});
 	//redirect back to campgrounds page
-	res.redirect("/campgrounds");
+	
 });
 
 // Add new campground form
